20b81f219b1a437b905b4fb8a3937c82852bf7a5
TosterLol <100956266+tosterlolz@users.noreply.github.com>
2025-05-10 16:40:42 +0200

    rewrite

--- src/client/kotlin/lol/toster/echorealm/EchoRealmClient.kt
+++
@@ -1,9 +0,0 @@
-package lol.toster.echorealm
-
-import net.fabricmc.api.ClientModInitializer
-
-object EchoRealmClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
---
+++ src/main/kotlin/lol/toster/echorealm/EchoRealmDataGenerator.kt
@@ -0,0 +1,9 @@
+package lol.toster.echorealm
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object EchoRealmDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
